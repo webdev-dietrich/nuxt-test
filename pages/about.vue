38041327017fd630c92dad0bc286d357ff52d008
Sascha Dietrich <info@webagentur-dietrich.de>
2025-04-04 12:58:30 +0000

    feat: add About page and link from Home page

---
+++ pages/about.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    <h1>About Page</h1>
+    <nuxt-link to="/">Go back to Home Page</nuxt-link>
+  </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -4,5 +4,6 @@
   <div class="container">
     <h1>Welcome to Nuxt 3</h1>
     <p>This is a simple example of a Nuxt 3 application.</p>
+    <nuxt-link to="/about">Go to About Page</nuxt-link>
   </div>
 </template>
